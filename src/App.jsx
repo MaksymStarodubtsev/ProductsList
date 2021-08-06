550e1b028e66a2cdc0456c4390fdaee09e964fa9
Maksym Starodubtsev <maksym.starodubtsev@gmail.com>
2021-08-06 23:58:19 +0300

    close

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,29 @@
 import React from 'react';
 import './App.scss';
 import './styles/general.scss';
-import { TodoList } from './components/TodoList';
-import { CurrentUser } from './components/CurrentUser';
+import { ProductsList } from './components/ProductsList';
+import { CurrentProduct } from './components/CurrentProduct';
+import productList from './components/api/products.json';
 
 class App extends React.Component {
   state = {
-    todos: [],
-    selectedUserId: 0,
+    products: [...productList],
+    selectedProductId: 0,
   };
 
   render() {
-    const { todos, selectedUserId } = this.state;
+    const { products, selectedProductId } = this.state;
 
     return (
       <div className="App">
         <div className="App__sidebar">
-          <TodoList todos={todos} />
+          <ProductsList products={products} />
         </div>
 
         <div className="App__content">
           <div className="App__content-container">
-            {selectedUserId ? (
-              <CurrentUser userId={selectedUserId} />
+            {selectedProductId ? (
+              <CurrentProduct userId={selectedProductId} />
             ) : 'No user selected'}
           </div>
         </div>
